94ead64969c46a31daebd946c48adafd8c392548
李智勇 <george510257@126.com>
2021-04-13 23:14:22 +0800

    update

--- project/framework/core/src/main/java/com/gls/framework/core/FrameworkCoreConfig.java
+++ project/framework/core/src/main/java/com/gls/framework/core/FrameworkCoreConfig.java
@@ -2,6 +2,7 @@ package com.gls.framework.core;
 
 import com.gls.framework.core.result.ResultProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,5 +14,6 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan
 @PropertySource(value = "classpath:application-framework.properties")
 @EnableConfigurationProperties(ResultProperties.class)
+@EnableDiscoveryClient
 public class FrameworkCoreConfig {
 }
