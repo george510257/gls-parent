6e6a9b448810d5c123806312f02f782c8dbd1b7a
李智勇 <george510257@126.com>
2021-05-07 14:25:31 +0800

    update

---
+++ project/job/dashboard/src/main/java/com/gls/job/dashboard/core/init/JobInitRunner.java
@@ -0,0 +1,35 @@
+package com.gls.job.dashboard.core.init;
+
+import com.gls.job.dashboard.core.job.BaseJob;
+import com.gls.job.dashboard.web.service.QuartzService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+
+/**
+ * @author george
+ */
+@Component
+public class JobInitRunner implements CommandLineRunner {
+
+    @Resource
+    private QuartzService quartzService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", 1);
+        quartzService.deleteJob("job", "test");
+        quartzService.addJob(BaseJob.class, "job", "test", "0 * * * * ?", map);
+
+        map.put("name", 2);
+        quartzService.deleteJob("job2", "test");
+        quartzService.addJob(BaseJob.class, "job2", "test", "10 * * * * ?", map);
+
+        map.put("name", 3);
+        quartzService.deleteJob("job3", "test2");
+        quartzService.addJob(BaseJob.class, "job3", "test2", "15 * * * * ?", map);
+    }
+}
--- project/job/dashboard/src/main/java/com/gls/job/dashboard/core/job/BaseJob.java
+++ project/job/dashboard/src/main/java/com/gls/job/dashboard/core/job/BaseJob.java
@@ -1,6 +1,7 @@
 package com.gls.job.dashboard.core.job;
 
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -15,5 +16,9 @@ public class BaseJob extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         log.info("hello,job!! :{}", context.getJobDetail().getKey());
+        // 获取参数
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        // 业务逻辑 ...
+        log.info("------spring boot quart zone job执行" + jobDataMap.get("name").toString() + "###############" + context.getTrigger());
     }
 }
